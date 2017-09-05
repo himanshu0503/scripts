ce3d16959eb112eed7252de6e5d67b8c90a0ca88
Himanshu Aggarwal <hemanshu0503@gmail.com>
2017-09-05 23:38:23 +0530

    Adds script to install zsh in mac

---
+++ setupZSH-mac.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -e
+
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
